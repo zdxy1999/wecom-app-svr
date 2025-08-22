3b889e8cf6b00fe198d3e2eefe6d928ff3d2f177
dichang <zhaodongxiaoyuan.z@lydaas.com>
2025-08-22 22:02:11 +0800

    feat:完善企微适配器

--- zdxy_adapter/adapter.py
+++ zdxy_adapter/adapter.py
@@ -2,6 +2,7 @@ import logging
 import os
 import sys
 
+import aiohttp
 import requests
 
 from cron_schedular.cron_job import cron_job, scheduler
@@ -12,52 +13,74 @@ load_dotenv()
 dify_url = os.getenv("DIFY_URL")
 dify_api_key = os.getenv("DIFY_API_KEY")
 
-
 def msg_handler(req_msg):
-    if req_msg.msg_type == 'text' and req_msg.content.strip() == 'help':
+    if req_msg.msg_type == 'text':
+        call_dify(user_input=req_msg.content, user="user")
+        # 不直接由该服务器直接回复，而是触发Dify流程由流程自动回复
         ret = RspTextMsg()
-        ret.content = f'msg_type: {req_msg.msg_type}, content: {req_msg.content}'
+        ret.content = '请稍后，答案正在生成中...'
         return ret
+
     if req_msg.msg_type == 'image':
-        ret = RspImageMsg(req_msg.to_user, req_msg.from_user, req_msg.media_id)
+        ret = RspTextMsg()
+        ret.msg_type = "text"
+        ret.content = "system: 用户发送了图片，系统目前无法处理图片"
         return ret
+
     if req_msg.msg_type == 'video':
-        ret = RspVideoMsg(req_msg.to_user, req_msg.from_user, req_msg.media_id, "视频标题", "视频描述")
+        ret = RspTextMsg()
+        ret.msg_type = "text"
+        ret.content = "system: 用户发送了图片，系统目前无法处理视频"
         return ret
 
     # 返回消息类型
     ret = RspTextMsg()
-    ret.content = f'msg_type: {req_msg.msg_type}, content: {req_msg.content}'
+    ret.content = "system: 用户发送了{}类型的消息，该消息类型暂无法处理".format(req_msg.msg_type)
 
     return ret
+
 
 
 def event_handler(req_msg):
     # TODO
     ret = RspTextMsg()
     if req_msg.event_type == 'add_to_chat':  # 入群事件处理
-        ret.content = f'msg_type: {req_msg.msg_type}\n群会话ID: {req_msg.chat_id}\n查询用法请回复: help'
+        ret.content = ""
     return ret
 
-@cron_job(hour=19, minute=2)
-def call_dify(user: str = "abc-123", content: str = "你好"):
+@cron_job(hour=9, minute=15)
+def daily_dify_call():
+    call_dify(user_input="每日开盘前提醒", user="system")
+
+async def call_dify(user_input: str="", user: str="default-user"):
+    url = os.getenv("DIFY_URL")
 
     headers = {
-        "Authorization": "Bearer {}".format(dify_api_key),
+        "Authorization": "Bearer {}".format(os.getenv("DIFY_API_KEY")),
         "Content-Type": "application/json"
     }
+
     payload = {
-        "inputs": {"content": content},
-        "response_mode": "blocking",
+        "inputs": {
+            "content": user_input
+        },
+        "response_mode": "streaming",
         "user": user
     }
 
-    response = requests.post(dify_url, headers=headers, json=payload)
+    connector = aiohttp.TCPConnector(ssl=False)
+    async with aiohttp.ClientSession(connector=connector) as session:
+        async with session.post(url, headers=headers, json=payload) as response:
+            print("状态码:", response.status)
+            response_text = await response.text()
+            print("响应内容:", response_text)
 
-    if response.status_code != 200:
-        logging.error("call dify failed \n {}".format(response.json()))
-    else:
-        logging.info("call dify success \n {}".format(response.json()))
+            # 如果返回是 JSON，可以直接解析
+            try:
+                json_response = await response.json()
+                print("JSON 响应:", json_response)
+            except Exception:
+                pass
 
 
 def main():
